d5d8e4b33e41fdf2032a9ec947aa69d0868e4f45
Brian Williams <heffergod@gmail.com>
2022-09-07 23:32:43 -0700

    completed coding challenge

--- chal-make-unique-string/makeUnique.js
+++ chal-make-unique-string/makeUnique.js
@@ -1,1 +1,13 @@
 // Write your solution below:
+
+const makeUnique = string => {
+    let letters = ''
+    for (i = 0; i < string.length; i++) {
+        if (!letters.includes(string[i])) {
+            letters += string[i]
+        }
+    }
+    return letters
+}
+
+console.log(makeUnique('helloworld'))
